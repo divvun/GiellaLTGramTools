950a1e768d3f7d950c5585fe6239b98a8ae7dce0
Børre Gaup <albbas@gmail.com>
2026-06-01 21:05:22 +0200

    Move tests to correct file

--- tests/test_error_annotated_sentence_to_grammar_error_annotated_sentence.py
+++ tests/test_error_annotated_sentence_to_grammar_error_annotated_sentence.py
@@ -10,6 +10,7 @@ from corpustools.error_types import ErrorType
 from giellaltgramtools.errordata import ErrorData
 from giellaltgramtools.grammar_error_annotated_sentence import (
     GrammarErrorAnnotatedSentence,
+    divvun_checker_output_to_grammar_error_annotated_sentence,
     error_annotated_sentence_to_grammar_error_annotated_sentence,
 )
 
@@ -87,7 +88,7 @@ from giellaltgramtools.grammar_error_annotated_sentence import (
             ),
             GrammarErrorAnnotatedSentence(
                 sentence="a e1 b e2.",
-                errors=(    
+                errors=(
                     ErrorData(
                         error_string="e1",
                         start=2,
@@ -252,3 +253,34 @@ def test_convert_error_annotated_sentence_to_grammar_error_annotated_sentence(
     assert (
         grammar_error_annotated_sentence == expected_grammar_error_annotated_sentence
     ), f'Test case "{name}" failed.'
+
+
+@pytest.mark.parametrize(
+    ("error_as_string", "wanted_sentence"),
+    [
+        (
+            r"""{"errs":[["la",17,19,"msyn-l-not-la","Dákkir kontevstan hiehpá \"la\" sajen adnet \"l\"",["l"],"\"Liehket\": \"l\" farra gå \"la\""]],"text":"Æjgátkonferánssa la ájnas tjåhkanimsadje gå galggá aktisattjat barggat oahppe/máná hárráj."}""",  # noqa: E501
+            "Æjgátkonferánssa {la}§{msyn-l-not-la|l} ájnas tjåhkanimsadje gå galggá "
+            "aktisattjat barggat oahppe/máná hárráj.",
+        ),
+        (
+            r"""{"errs":[["dáhttun",14,21,"real-DerNomActSgGen-PrfPrc","Sátni šaddá eará go oaivvilduvvo.",["dáhtton"],"Čállinmeattáhus dán oktavuođas"]],"text":"Eat lean olus dáhttun."}""",  # noqa: E501
+            "Eat lean olus {dáhttun}§{real-DerNomActSgGen-PrfPrc|dáhtton}.",
+        ),
+        (
+            r"""{"errs":[["gelddolas",0,9,"typo","Hápmi ii leat sátnelisttus.",["gelddolaš","gulddolaš","gilddolaš","guldalas","gelddolačča","gáldolaš","galddalaš","gielddalaš","gealdálas","gulddalmas"],"Čállinmeattáhus"]],"text":"gelddolas"}""",  # noqa: E501
+            "{gelddolas}${typo|gelddolaš,gulddolaš,gilddolaš,guldalas,gelddolačča,gáldolaš,galddalaš,gielddalaš,gealdálas,gulddalmas}",
+        ),
+        (
+            r"""{"errs":[["valjis",19,25,"lex-valjis-valljugas","lex-valjis-valljugas",["valljugas"],"lex-valjis-valljugas"]],"text":"Guovdageainnus lea valjis ja girjás kultureallin."}""",  # noqa: E501
+            "Guovdageainnus lea {valjis}§{lex-valjis-valljugas|valljugas} ja girjás "
+            "kultureallin.",
+        ),
+    ],
+)
+def test_divvun_checker_to_markup(error_as_string, wanted_sentence):
+    error_data = divvun_checker_output_to_grammar_error_annotated_sentence(
+        error_as_string
+    )
+
+    assert error_data.to_manual_markup() == wanted_sentence
